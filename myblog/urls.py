a83409a01cf263a6de90a35badef388f064c145b
abi <rasheed.abiola3@gmail.com>
2017-10-28 17:32:40 +0100

    Created URL patterns for index, about, blog and feedback

--- myblog/urls.py
+++ myblog/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
+    url(r'^', TemplateView.as_view(template_name="index.html"), name='index'),
+    url(r'^about/$', TemplateView.as_view(template_name="about.html"), name='about'),
+    url(r'^blog/$', TemplateView.as_view(template_name="blog.html"), name='blog'),
+    url(r'^feedback/$', TemplateView.as_view(template_name="feedback.html"), name='feedback'),
     url(r'^admin/', admin.site.urls),
 ]
