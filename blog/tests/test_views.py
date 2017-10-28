d1fedaeb0965dfab3c41c78de216e918af2a15ae
abi <rasheed.abiola3@gmail.com>
2017-10-28 20:17:39 +0100

    Created test case for views (index, about, blog and feedback)

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -16,3 +16,15 @@ class BlogViewTestCase(TestCase):
         self.assertEqual(path, reverse(path_name))
         self.assertEqual(get_template(template_name).template.name, template_name)
         self.assertIsInstance(get_template(template_name).render(), SafeText)
+
+    def test_index_page(self):
+        self.generic_templateview_tester('/', 'index', 'index.html')
+
+    def test_about(self):
+        self.generic_templateview_tester("/about/", 'about', "about.html")
+
+    def test_feedback_blog(self):
+        self.generic_templateview_tester('/blog/', 'blog', 'blog.html')
+
+    def test_feedback(self):
+        self.generic_templateview_tester("/feedback/", "feedback", "feedback.html")
