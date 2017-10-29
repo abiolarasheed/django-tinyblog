ccf35843c97950ff175f8441062465e6c30f26b5
abi <rasheed.abiola3@gmail.com>
2017-10-29 18:35:34 +0000

    Created EntryViewTestCase

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
+from django.contrib.auth import get_user_model
+from django.template.loader import get_template
 from django.test import TestCase
 from django.urls.base import reverse
-from django.template.loader import get_template
 from django.utils.safestring import SafeText
+
+from ..models import Entry
 
 
 class BlogViewTestCase(TestCase):
@@ -28,3 +31,26 @@ class BlogViewTestCase(TestCase):
 
     def test_feedback(self):
         self.generic_template_view_tester("/feedback/", "feedback", "feedback.html")
+
+
+class EntryViewTestCase(TestCase):
+    def setUp(self):
+        self.author, auth_created = get_user_model().\
+            objects.get_or_create(email="iamatest@gmail.com", username="iamatest")
+        self.blog_title = "Test blog Title"
+        self.blog_body = "This is my test blog"
+        self.entry = Entry.objects.get_or_create(title=self.blog_title,
+                                                 body=self.blog_body,
+                                                 author=self.author)[0]
+
+    def test_basic_view(self):
+        response = self.client.get(self.entry.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+
+    def test_title_in_entry(self):
+        response = self.client.get(self.entry.get_absolute_url())
+        self.assertContains(response, self.entry.title)
+
+    def test_body_in_entry(self):
+        response = self.client.get(self.entry.get_absolute_url())
+        self.assertContains(response, self.entry.body)
