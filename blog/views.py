4124064d703d3d74ed28e999bf4db5b077d62673
abi <rasheed.abiola3@gmail.com>
2017-10-29 18:03:52 +0000

    Created EntryDetail

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Entry
 
-# Create your views here.
+
+class EntryDetail(DetailView):
+    model = Entry
