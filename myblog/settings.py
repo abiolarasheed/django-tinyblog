d8fbea6a984e61e19e184ba2ad919707a18a528a
abi <rasheed.abiola3@gmail.com>
2017-10-29 11:10:55 +0000

    Added django-taggit to settings

--- myblog/settings.py
+++ myblog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'taggit',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(DIR, 'static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(DIR, 'media/')
+
+#  django-taggit settings
+TAGGIT_CASE_INSENSITIVE = True
