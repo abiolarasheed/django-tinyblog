3034c41eade5706e1cdb46a13bb310148fc2b8d0
abi <rasheed.abiola3@gmail.com>
2017-10-29 02:58:18 +0000

    Updates settings  media and staic settings

--- myblog/settings.py
+++ myblog/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -117,5 +118,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATICFILES_DIRS = [
+    os.path.join(DIR, 'assets/'),
+]
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(DIR, 'static/')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(DIR, 'media/')
