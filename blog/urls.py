87c50d0ffa0ccd4ce6a43babf27e2bef4db1dee5
abi <rasheed.abiola3@gmail.com>
2017-10-30 14:00:21 +0000

    Refactored blog url pattern for post list view

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^post/$', views.EntryListView.as_view(), name='entry_list'),
     url(r'^(?P<slug>[-\w]+)/$', views.EntryDetail.as_view(), name='entry_detail'),
-    url(r'^post(?P<page>[0-9]+)/$', views.EntryListView.as_view(), name='entry_list')
 ]
