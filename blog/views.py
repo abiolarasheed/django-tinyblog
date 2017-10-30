c5ad1a266e8025b1213fe02c0473c60e455abcdc
abi <rasheed.abiola3@gmail.com>
2017-10-30 10:34:37 +0000

    Updated EntryDetail to process context and set context varibale and template name

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,11 @@ from .models import Entry
 
 
 class EntryDetail(DetailView):
+    template_name = 'entry_detail.html'
+    context_object_name = 'entry'
     model = Entry
+
+    def get_context_data(self, **kwargs):
+        context = super(EntryDetail, self).get_context_data(**kwargs)
+        context['title'] = context['entry'].title
+        return context
