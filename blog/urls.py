040ac2b3ef6427231aecf3bf45428c5b82cc39eb
abi <rasheed.abiola3@gmail.com>
2017-10-30 12:10:02 +0000

    Created url pattern for EntryListView

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^(?P<slug>[-\w]+)/$', views.EntryDetail.as_view(), name='entry_detail'),
+    url(r'^post(?P<page>[0-9]+)/$', views.EntryListView.as_view(), name='entry_list')
 ]
