c5e11c07447f2c24d4942f71aa5cec4e7e783a62
abi <rasheed.abiola3@gmail.com>
2017-10-30 10:33:04 +0000

    Refactored EntryDetail view url pattern to accept slug

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<pk>\d+)/$', views.EntryDetail.as_view(), name='entry_detail'),
+    url(r'^(?P<slug>[-\w]+)/$', views.EntryDetail.as_view(), name='entry_detail'),
 ]
