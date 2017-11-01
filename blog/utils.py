5fdf5e9ce714ed106511add3baf49b3675f9eb94
abi <rasheed.abiola3@gmail.com>
2017-11-01 22:28:58 +0000

    Added logic to ajax_required

--- blog/utils.py
+++ blog/utils.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
+from django.http.response import JsonResponse
 
 
 def ajax_required(view_function):
     """
     AJAX request required decorator
-      use it in your views:
+    use it in your views:
     """
-    pass
+    def wrap(request, *args, **kwargs):
+        if not request.is_ajax():
+            return JsonResponse({'message': "Bad Request"},
+                                status=400)
+        return view_function(request, *args, **kwargs)
+    wrap.__doc__ = view_function.__doc__
+    wrap.__name__ = view_function.__name__
+    return wrap
