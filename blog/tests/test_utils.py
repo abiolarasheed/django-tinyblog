cd8b1b33d37d6348ad2a21d6ccc371712306c2c1
abi <rasheed.abiola3@gmail.com>
2017-11-01 22:22:35 +0000

    Created AjaxRequiredTestCase.test_is_ajax_false

--- blog/tests/test_utils.py
+++ blog/tests/test_utils.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+import json
 from django.test import TestCase
 from unittest.mock import MagicMock, Mock
+from blog.utils import ajax_required
 
 
 class AjaxRequiredTestCase(TestCase):
@@ -19,3 +21,25 @@ class AjaxRequiredTestCase(TestCase):
         # Add the __name__ and __doc__ to view function.
         self.test_view.__doc__ = """Doc for my test view."""
         self.test_view.__name__ = 'test_view'
+
+    def test_is_ajax_false(self):
+        """ajax_required decorator called from none ajax client."""
+
+        # Setup.
+        attributes = {'is_ajax.return_value': False}
+        self.ajax_request.configure_mock(**attributes)
+
+        # Run.
+        decorated = ajax_required(self.test_view)
+        response = decorated(self.ajax_request)
+
+        # Check.
+
+        # View function was not called.
+        self.test_view.assert_not_called()
+
+        # Decorator response.status_code was returned.
+        self.assertEqual(response.status_code, 400)
+
+        # Decorator response.content.
+        self.assertEqual(json.loads(response.content)['message'], "Bad Request")
