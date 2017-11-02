e3669936e5c8ccd67fbb5895ad750d8d9c16d8d8
abi <rasheed.abiola3@gmail.com>
2017-11-02 17:10:44 +0000

    Renamed app to tinyblog

--- tinyblog/settings.py
+++ tinyblog/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'myblog.urls'
+ROOT_URLCONF = 'tinyblog.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myblog.wsgi.application'
+WSGI_APPLICATION = 'tinyblog.wsgi.application'
 
 
 # Database
