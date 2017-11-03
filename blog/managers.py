64ef20f40dfb89463bfa2ac3e06edf25d9abc274
abi <rasheed.abiola3@gmail.com>
2017-11-03 21:10:26 +0000

    Added as_manager method to PublishedEntryQuerySet

--- blog/managers.py
+++ blog/managers.py
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*-
+import types
 from django.db import models
 
 
 class PublishedEntryQuerySet(models.QuerySet):
     """QuerySet that returns only published Entries."""
-    pass
+
+    @classmethod
+    def as_manager(cls):
+
+        def get_queryset(self):
+            return PublishedEntryQuerySet(self.model, using=self._db).filter(is_published=True)
+
+        #  Get the manger form the QuerySet class as_manager()
+        manager = super(PublishedEntryQuerySet, cls).as_manager()
+
+        # Replace the manager's get_queryset method with the one we have above
+        manager.get_queryset = types.MethodType(get_queryset, manager)
+        return manager
