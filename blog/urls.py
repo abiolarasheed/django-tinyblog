f9d4056b2c594292e5bc392c569a04325ea87deb
abi <rasheed.abiola3@gmail.com>
2017-11-05 22:28:20 +0000

    Created url pattern for image_update

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ from .utils import ajax_required
 urlpatterns = [
     url(r'^image/(?P<pk>[0-9]+)/$', views.ImageDetailView.as_view(), name='image_detail'),
     url(r'^image/create/$', views.ImageCreateView.as_view(), name='image_create'),
+    url(r'^image-update/(?P<pk>[0-9]+)/$', views.EntryUpdateView.as_view(), name='image_update'),
     url(r'^create/$', views.EntryCreateView.as_view(), name='entry_create'),
     url(r'^list/$', views.EntryListView.as_view(), name='entry_list'),
     url(r'^search/$', ajax_required(views.JsonSearchView()), name='navbar_search'),
