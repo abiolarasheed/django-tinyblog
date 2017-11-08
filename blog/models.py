14006ada387290fc65af06d185e006f20acb273a
abi <rasheed.abiola3@gmail.com>
2017-11-08 13:17:58 +0000

    Added views to Entry model and added a few more fields

--- blog/models.py
+++ blog/models.py
@@ -24,6 +24,7 @@ class Entry(models.Model):
     poster = models.ImageField(null=True, blank=True, upload_to=
                                FileUploader(os.path.join('entry', 'poster')))
     is_published = models.BooleanField(default=False)
+    views = models.PositiveIntegerField(default=1)  # we will change this later and write a better one later
     tags = TaggableManager()
     objects = models.Manager()
     published = PublishedEntryQuerySet.as_manager()
@@ -46,12 +47,36 @@ class Entry(models.Model):
             num += 1
         return unique_slug
 
+    def get_similar_post(self):
+        return self.__class__.published.\
+            filter(tags__in=self.tags.all()).exclude(id=self.id).distinct()
+
     def get_absolute_url(self):
         return reverse('entry_detail', kwargs={'slug': self.slug})
 
     def as_json(self):
         return {"title": self.title,
                 "url": self.get_absolute_url()}
+
+    def get_poster(self):
+        try:
+            return self.poster.url
+        except (AttributeError, ValueError):
+            return "http://via.placeholder.com/318x224"
+
+    def count_comments(self):
+        return 0
+
+    def count_words_in_text(self, word_length=5):
+        total_words = 0
+        for current_text in self.body:
+            total_words += len(current_text) / word_length
+        return total_words
+
+    def estimate_reading_time(self):
+        words_per_min = 200  # Word per min
+        total_words = self.count_words_in_text()
+        return round(total_words / words_per_min)
 
     def save(self, *args, **kwargs):
         if self.is_published and self.published_date is None:
