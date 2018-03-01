73202970cf0167a61a6a0e6bab437e02b91d6b5b
abi <rasheed.abiola3@gmail.com>
2018-03-01 18:23:27 +0000

    pep8

--- accounts/urls.py
+++ accounts/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     url(r'^password_change/$',
         PasswordChangeView.as_view(template_name='password_change_form.html'),
         name='password_change'),
+
     url(r'^password_change/done/$',
         PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
         name='password_change_done'),
@@ -34,12 +35,15 @@ urlpatterns = [
     url(r'^password_reset/$',
         PasswordResetView.as_view(template_name='password_reset_form.html'),
         name='password_reset'),
+
     url(r'^password_reset/done/$',
         PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
         name='password_reset_done'),
+
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
         name='password_reset_confirm'),
+
     url(r'^reset/done/$',
         PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
         name='password_reset_complete'),
