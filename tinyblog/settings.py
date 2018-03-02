0b55ac850d3b0b9f56ff0228918fe14e5de35ba7
abi <rasheed.abiola3@gmail.com>
2018-03-02 23:55:02 +0000

    Postgresql integration

--- tinyblog/settings.py
+++ tinyblog/settings.py
@@ -75,15 +75,16 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'tinyblog.wsgi.application'
-
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['BLOG_DATABASE_NAME'],
+        'USER': os.environ['BLOG_DATABASE_USER'],
+        'PASSWORD': os.environ['BLOG_DATABASE_PASSWORD'],
+        'CONN_MAX_AGE':  None,
     }
 }
 
