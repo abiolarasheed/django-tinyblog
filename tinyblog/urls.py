78e5a4289a7d2b58c1bdb2168048594166890a7f
abi <rasheed.abiola3@gmail.com>
2018-03-04 07:05:34 +0000

    Created sitemap URL

--- tinyblog/urls.py
+++ tinyblog/urls.py
@@ -1,23 +1,36 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import include
-from django.urls import path, re_path
-from django.contrib import admin
 from django.conf import settings
+from django.conf.urls import include
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
+from django.urls import path, re_path
 from django.views.generic import TemplateView
 
+import accounts.urls
+import analytics.urls
 import blog.urls
 import blog.views as blog_views
-import analytics.urls
+import blog.sitemaps as blog_sitemaps
+
+
+sitemaps = {
+    'static': blog_sitemaps.StaticViewSitemap,
+    "blogs": blog_sitemaps.BlogSitemap,
+}
 
 
 urlpatterns = [
-    path('about/', TemplateView.as_view(template_name="about.html"), name='about'),
+    path('about/', TemplateView.as_view(template_name="about.html"),
+         name='about'),
     path('admin/', admin.site.urls),
-    path('analytics/',include(analytics.urls)),
-    path('feedback/', TemplateView.as_view(template_name="feedback.html"), name='feedback'),
-    re_path('analytics/', include('accounts.urls')),
+    path('analytics/', include(analytics.urls)),
     re_path('blog/', include(blog.urls)),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+     name='django.contrib.sitemaps.views.sitemap'),
+    path('feedback/', TemplateView.as_view(template_name="feedback.html"),
+         name='feedback'),
+    re_path('', include(accounts.urls)),
 ]
 
 
