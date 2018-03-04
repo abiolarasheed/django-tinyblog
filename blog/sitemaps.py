f6e8928a8de9c3e4369074bfef87e51ead2aa26e
abi <rasheed.abiola3@gmail.com>
2018-03-04 07:04:09 +0000

    Create blog/sitemap

---
+++ blog/sitemaps.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from django.contrib import sitemaps
+from django.urls import reverse
+
+from blog.models import Entry
+
+
+class StaticViewSitemap(sitemaps.Sitemap):
+    priority = 0.5
+    changefreq = 'weekly'
+
+    def items(self):
+        return ['about', 'feedback',
+                'index', 'entry_list']
+
+    def location(self, item):
+        return reverse(item)
+
+
+class BlogSitemap(sitemaps.Sitemap):
+    changefreq = "never"
+    priority = 0.9
+
+    def items(self):
+        return Entry.published.all().order_by('-modified_at')
+
+
+    def lastmod(self, obj):
+        return obj.modified_at
