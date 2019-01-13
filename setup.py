3b57a68cf47089676c346f59a495ecb99cc13d50
Abiola Rasheed <abiola@passthekeys.co.uk>
2019-01-13 11:50:11 +0000

    Dropped requirements.txt and integrated Pipenv into setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@
 from __future__ import unicode_literals
 
 from distutils.core import setup
+
+from pipenv.project import Project
+from pipenv.utils import convert_deps_to_pip
+
 import tinyblog
 
-required = []
-with open("requirements.txt") as f:
-    required.extend(f.read().splitlines())
+
+project_file = Project(chdir=False).parsed_pipfile
+requirements = convert_deps_to_pip(project_file["packages"], r=False)
 
 
 setup(
@@ -20,7 +24,7 @@ setup(
     description="A mini django blogging app written in python3.6+.",
     long_description=open("README.rst").read(),
     keywords=["blog", "django", "tinyblog", "tiny"],
-    install_requires=required,
+    install_requires=requirements,
     include_package_data=True,
     zip_safe=False,
     classifiers=[
