835cc1006460f73fb5d0c3650126c653e2cbfc42
Abiola Rasheed <rasheed.abiola3@gmail.com>
2020-07-01 00:40:09 +0100

    Updated cache url for docker-compose

--- tinyblog/settings.py
+++ tinyblog/settings.py
@@ -128,7 +128,7 @@ SESSION_CACHE_ALIAS = "default"
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": os.environ.get("CACHE_URL", "redis://127.0.0.1:6379/1"),
+        "LOCATION": os.environ.get("CACHE_URL", "redis://redis/1"),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
             "COMPRESSOR": "django_redis.compressors.zlib.ZlibCompressor",
